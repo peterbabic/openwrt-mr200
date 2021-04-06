99e999b9e020499d39874c0fc287e12c385fb74e
Peter Babič <peter@peterbabic.dev>
2021-04-06 15:21:34 +0200

    fix sha256sum recheck

--- make.sh
+++ make.sh
@@ -4,24 +4,26 @@ version="19.07.7"
 fprintUrl="https://openwrt.org/docs/guide-user/security/signatures"
 dlUrl="https://downloads.openwrt.org/releases/$version/targets/ramips/mt7620/"
 folderName="openwrt-imagebuilder-$version-ramips-mt7620.Linux-x86_64"
+archive="$folderName.tar.xz"
 
 mkdir -p tmp/ build/
 cd tmp/
 
 sudo pacman -q -S --needed base-devel ncurses zlib gawk git gettext openssl libxslt wget unzip python gnupg
 
+sysupgradeFile="openwrt-$version-ramips-mt7620-ArcherMR200-squashfs-sysupgrade.bin"
 sumsFile="sha256sums"
 ascFile="$sumsFile.asc"
 
-rm $sumsFile*
-wget -q "$dlUrl/$sumsFile"
-wget -q "$dlUrl/$ascFile"
+wget -q "$dlUrl/$ascFile" -O "$ascFile"
+wget -q "$dlUrl/$sumsFile" -O "$sumsFile"
 
 gpgOutput=$(gpg --status-fd 1 --auto-key-retrieve --with-fingerprint --verify "$ascFile") 
 
 if [ $? -eq 0 ]; then
 	echo "SIGNATURE VERIFIED"
 else
+	echo "SIGNATURE INVALID. Exitting."
 	exit 1
 fi
 
@@ -33,18 +35,33 @@ curl -s "$fprintUrl" | grep -o "$fprintFmt"
 if [ $? -eq 0 ]; then
 	echo "FINGERPRINT VERIFIED"
 else
+	echo "FINGERPRINT INVALID. Exiting."
 	exit 1
 fi
 
+touch -a "$sysupgradeFile"
+touch -a "$archive"
 sha256sum --ignore-missing -c "$sumsFile" 
 
 if [ $? -eq 0 ]; then
-	echo "SHA256SUM VERIFIED, no need to redownload"
+	echo "SHA256SUM VERIFIED"
 else
-	rm -rf $folderName*
-	archive="$folderName.tar.xz"
-	wget "$dlUrl/$archive"
+	echo "SHA256SUM INVALID. Attempting new download."
+
+	wget "$dlUrl/$archive" -O "$archive"
+	wget "$dlUrl/$sysupgradeFile" -O "$sysupgradeFile"
+
+	rm -rf "$folderName"
 	tar xJf "$archive"
+
+	sha256sum --ignore-missing -c "$sumsFile" 
+
+	if [ $? -eq 0 ]; then
+		echo "SHA256SUM VERIFIED."
+	else
+		echo "SHA256SUM INVALID. Exiting."
+		exit 1
+	fi
 fi
 
 cd "$folderName"
